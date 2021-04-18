d87009cf7e8e8bca620fb68ca47e0dc9f45d0f68
yedilio <sarsen.yedil@gmail.com>
2021-04-18 17:40:05 +0600

    's'

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Game/>
   </div>
 </template>
 
 <script>
-
+  import Game from "./components/Game";
 export default {
   name: 'App',
   components: {
+    Game
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  div {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
